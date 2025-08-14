f0dd5f52c19987e63414f5db0c6cfe4d1101c0d3
codewithfrancis <offor919@gmail.com>
2025-08-14 22:58:22 +0100

    add creation of task business logic

--- src/task /task.service.ts
+++ src/task /task.service.ts
@@ -1,13 +1,18 @@
 import { PrismaService } from "src/database/prisma/prisma.service";
 import { Injectable } from "@nestjs/common";
+import { CreateTaskDto } from "./dto/task.dto";
 
 @Injectable()
 export class TaskService{
     constructor(private readonly prisma: PrismaService){}
 
-async createTask(dto, fileUrl, userId){
-    return await this.prisma.create({
-        
+async createTask(dto: CreateTaskDto, fileUrl, userId){
+    return await this.prisma.task.create({
+     data: {
+        ...dto,
+        fileUrl: fileUrl,
+        userId: userId
+     }
     })
 }
 }
