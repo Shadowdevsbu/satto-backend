e7a2e5426abd5078037d573ed9aa21a46eaf6d47
codewithfrancis <offor919@gmail.com>
2025-08-14 22:50:17 +0100

    upadet task schema

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthService } from './auth.service';
 import { EmailService } from './email/email.service';
 import { GoogleStrategy } from './strategy/google.strategy';
 import { jwtStrategy } from './strategy/jwt.strategy';
+import { jwtGuard } from './guards/jwt.guards';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from 'src/constant';
 import { PassportModule } from '@nestjs/passport';
@@ -17,6 +18,7 @@ import { PassportModule } from '@nestjs/passport';
     signOptions: {expiresIn:jwtConstants.expiresIn}
   })],
   controllers: [AuthController],
-  providers: [AuthService, EmailService, GoogleStrategy],
+  providers: [AuthService, EmailService, GoogleStrategy, jwtStrategy],
+  exports: [jwtStrategy, jwtGuard]
 })
 export class AuthModule {}
--- src/task /task.service.ts
+++ src/task /task.service.ts
@@ -1,1 +1,13 @@
 import { PrismaService } from "src/database/prisma/prisma.service";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class TaskService{
+    constructor(private readonly prisma: PrismaService){}
+
+async createTask(dto, fileUrl, userId){
+    return await this.prisma.create({
+        
+    })
+}
+}
